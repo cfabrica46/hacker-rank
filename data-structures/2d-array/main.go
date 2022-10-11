4ba4a905abc30b1a640b2d59c13b6de949b59c98
César Caycho <cfabrica46@gmail.com>
2022-10-10 23:07:27 -0500

    [fix]: 2d array fix caser negative sum

--- data-structures/2d-array/main.go
+++ data-structures/2d-array/main.go
@@ -1,50 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	arr := [][]int32{
-		{1, 1, 1, 0, 0, 0},
-		{0, 1, 0, 0, 0, 0},
-		{1, 1, 1, 0, 0, 0},
+		{-1, -1, 0, -9, -2, -2},
+		{-2, -1, -6, -8, -2, -5},
+		{-1, -1, -1, -2, -3, -4},
 		//---
-		{0, 0, 2, 4, 4, 0},
-		{0, 0, 0, 2, 0, 0},
-		{0, 0, 1, 2, 4, 0},
+		{-1, -9, -2, -4, -4, -5},
+		{-7, -3, -3, -2, -9, -9},
+		{-1, -3, -1, -2, -4, -5},
 	}
 
 	fmt.Println(hourglassSum(arr))
 }
 
 func hourglassSum(arr [][]int32) int32 {
-	var max int32
+	// var max int32
+	var arrSum []int
 
-	fmt.Println("----Start----")
-	/* var height, weight int
-
-	height = len(arr)
-	weight = len(arr[0])
-
-	fmt.Println(height, weight) */
+	// fmt.Println("----Start----")
 
 	for i := 0; i < len(arr)-2; i++ {
 		for j := 0; j < len(arr[0])-2; j++ {
 			auxSum := sum(arr, i, j)
 
-			if max < auxSum {
-				max = auxSum
-			}
-			/* fmt.Println("*******")
-			fmt.Println(arr[i][j], arr[i][j+1], arr[i][j+2])
-			fmt.Println(arr[i+1][j+1])
-			fmt.Println(arr[i+2][j], arr[i+2][j+1], arr[i+2][j+2])
-			fmt.Println("*******") */
+			/*
+				Fails because max init with 0 value and auxSum is minor of 0 for example = -6
+				needs other solution
+
+				if max < auxSum {
+					max = auxSum
+				}
+			*/
+
+			arrSum = append(arrSum, int(auxSum))
+			sort.Ints(arrSum)
+
 		}
-		// fmt.Println("")
 	}
 
-	fmt.Println("----END----")
-	return max
+	// fmt.Println("----END----")
+	return int32(arrSum[len(arrSum)-1])
 }
 
 func sum(arr [][]int32, i, j int) int32 {
