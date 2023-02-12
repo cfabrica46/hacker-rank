4ad23012908d3bf235466812387647c0f81157de
César Caycho <cfabrica46@gmail.com>
2023-02-11 23:38:29 -0500

    feat: pangrams

--- preparation-kit/week2/pangrams/pangrams.go
+++ preparation-kit/week2/pangrams/pangrams.go
@@ -1,7 +1,33 @@
 package pangrams
 
-func Pangrams(s string) string {
-	// Write your code here
+import (
+	"strings"
+)
 
-	return ""
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+)
+
+func Pangrams(s string) string {
+	if len(s) < len(alphabet) {
+		return "not pangram"
+	}
+
+	s = strings.ToLower(s)
+
+	letter := make(map[rune]bool)
+
+	for _, v := range s {
+		if v >= 'a' && v <= 'z' {
+			letter[v] = true
+		}
+	}
+
+	for c := 'a'; c <= 'z'; c++ {
+		if !letter[c] {
+			return "not pangram"
+		}
+	}
+
+	return "pangram"
 }
