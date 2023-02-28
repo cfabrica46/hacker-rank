ab7f0a0e517fe034d5d565b8fa47494375c099aa
César Caycho <cfabrica46@gmail.com>
2023-02-28 14:39:30 -0500

    fix: migratory birds

--- preparation-kit/week3/migratory-birds/migratory_birds.go
+++ preparation-kit/week3/migratory-birds/migratory_birds.go
@@ -1,10 +1,6 @@
 package migratorybirds
 
-import (
-	"fmt"
-)
-
-func MigratoryBirds(arr []int32) (result int32) {
+func MigratoryBirds(arr []int32) int32 {
 	myMap := make(map[int32]int32)
 
 	for i := range arr {
@@ -16,13 +12,10 @@ func MigratoryBirds(arr []int32) (result int32) {
 
 	for i := range myMap {
 		if myMap[i] >= maxValue {
-			fmt.Println(myMap[i], maxValue, i, maxID)
-
-			if i < maxID {
+			if myMap[i] > maxValue || i < maxID {
 				maxID = i
 				maxValue = myMap[i]
 			}
-			fmt.Println("->", myMap[i], maxValue, i, maxID)
 		}
 	}
 
