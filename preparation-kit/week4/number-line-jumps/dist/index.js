d1c68a0153aed1ab252843a8e94e21afa9a030e7
César Caycho <cfabrica46@gmail.com>
2023-05-10 18:25:28 -0500

    feat: number-line-jumps ts

---
+++ preparation-kit/week4/number-line-jumps/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+function kangaroo(x1, v1, x2, v2) {
+    if (v1 === v2) {
+        return "NO";
+    }
+    const jumps = (x2 - x1) / (v1 - v2);
+    console.log(x2 - x1, v1 - v2, jumps);
+    if (Number.isInteger(jumps) && jumps >= 0) {
+        return "YES";
+    }
+    return "NO";
+}
+function main() {
+    console.log(kangaroo(0, 2, 5, 3));
+    console.log(kangaroo(0, 3, 4, 2));
+    console.log(kangaroo(5, 1, 4, 2));
+    console.log(kangaroo(11, 2, 2, 4));
+}
+main();
+// 0 - 3 - 6 - 9 - 12
+// 4 - 6 - 8 - 10 - 12
+// ----
+// 5 - 6
+// 4 - 6
